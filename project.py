e178b9cdccbb7379917c5ad2061eb3722fc055aa
gokkulasudanr92 <gokkulasudanr92@gmail.com>
2017-11-26 23:15:24 -0500

    SelectKBest Features for model prediction 	1. Need to remove comments 	2. Need to look into some more models

--- project.py
+++ project.py
@@ -5,10 +5,14 @@ from time import time
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_selection import SelectFromModel
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
 from sklearn.linear_model import RidgeClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
@@ -22,6 +26,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils.extmath import density
 from sklearn import metrics
 
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+
+# Remove the quotes, header and footer from the dataset
+print("Loading 20 newsgroups dataset for categories:")
 # List of 20 categories
 categories = [
         'sci.crypt', 
@@ -45,58 +54,30 @@ categories = [
         'comp.sys.mac.hardware', 
         'comp.windows.x'
     ]
-
-# Remove the quotes, header and footer from the dataset
 remove = ('headers', 'footers', 'quotes')
-
-print("Loading 20 newsgroups dataset for categories:")
-
-data_train = fetch_20newsgroups(subset='train', categories=categories,
-                                shuffle=True, random_state=42,
-                                remove=remove)
-
-data_test = fetch_20newsgroups(subset='test', categories=categories,
-                               shuffle=True, random_state=42,
-                               remove=remove)
-
-print("Data loaded with train size: " + str(len(data_train.data)) + " and test size: " + str(len(data_test.data)))
-target_names = data_train.target_names
-
-# Splitting the target labels for train and test data
-y_train, y_test = data_train.target, data_test.target
+dataset = fetch_20newsgroups(subset='all', categories=categories, shuffle=True, random_state=42, remove=remove)
+print("Data loaded with size: " + str(len(dataset.data)))
+target_names = dataset.target_names
 
 # Let us start with vectorization
-vectorizer_train = TfidfVectorizer(sublinear_tf=True, 
-                                    max_df=0.75,
-                                    stop_words='english')
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
 
-vectorizer_test = TfidfVectorizer(sublinear_tf=True, 
-                                    max_df=0.75,
-                                    stop_words='english')
-x_train = vectorizer_train.fit_transform(data_train.data)
-x_test = vectorizer_test.fit_transform(data_test.data)
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+        return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
 
-#print(vectorizer_train.get_feature_names())
-#print
-#print
-#print(vectorizer_train.vocabulary_)
+stemmed_count_vect = StemmedCountVectorizer(stop_words='english', max_df=0.5)
+data_count = stemmed_count_vect.fit_transform(dataset.data)
+
+transformer = TfidfTransformer(sublinear_tf=True)
+x = transformer.fit_transform(data_count)
 
 ## First way of dimension reduction using Chi Square Test
 feature_size = 11000
-feature_names = vectorizer_train.get_feature_names()
 print("Extracting %d best features by a chi-squared test:" % feature_size)
 ch2 = SelectKBest(chi2, k=feature_size)
-new_x_train = ch2.fit_transform(x_train, y_train)
-new_x_test = ch2.fit_transform(x_test, y_test)
-feature_names = [feature_names[i] for i in ch2.get_support(indices=True)]
-
-if feature_names:
-    feature_names = np.asarray(feature_names)
-
-#print(feature_names)
-#print()
-#print()
-#print(len(feature_names))
+X = ch2.fit_transform(x, dataset.target)
 
 def trim(s):
     """Trim string to fit on terminal (assuming 80-column display)"""
@@ -105,46 +86,66 @@ def trim(s):
 # Benchmark classifiers
 def benchmark(clf):
     print('_' * 80)
-    print("Training: ")
     print(clf)
-    t0 = time()
-    clf.fit(new_x_train, y_train)
-    train_time = time() - t0
-    print("train time: %0.3fs" % train_time)
+    #t0 = time()
+    #clf.fit(new_x_train, y_train)
+    #train_time = time() - t0
+    #print("train time: %0.3fs" % train_time)
 
-    t0 = time()
-    pred = clf.predict(new_x_test)
-    test_time = time() - t0
-    print("test time:  %0.3fs" % test_time)
+    #t0 = time()
+    #pred = clf.predict(new_x_test)
+    #test_time = time() - t0
+    #print("test time:  %0.3fs" % test_time)
 
-    score = metrics.accuracy_score(y_test, pred)
-    print("accuracy:   %0.3f" % score)
+    #score = metrics.accuracy_score(y_test, pred)
+    #print("accuracy:   %0.3f" % score)
 
-    if hasattr(clf, 'coef_'):
-        print("dimensionality: %d" % clf.coef_.shape[1])
-        print("density: %f" % density(clf.coef_))
+    #if hasattr(clf, 'coef_'):
+    #    print("dimensionality: %d" % clf.coef_.shape[1])
+    #    print("density: %f" % density(clf.coef_))
 
         
-        print("top 10 keywords per class:")
-        for i, label in enumerate(target_names):
-            top10 = np.argsort(clf.coef_[i])[-10:]
-            print(trim("%s: %s" % (label, " ".join(feature_names[top10]))))
-        print()
+    #    print("top 10 keywords per class:")
+    #    for i, label in enumerate(target_names):
+    #        top10 = np.argsort(clf.coef_[i])[-10:]
+            #print(trim("%s: %s" % (label, " ".join(feature_names[top10]))))
+    #    print()
 
-    print("classification report:")
-    print(metrics.classification_report(y_test, pred, target_names=target_names))
-    print("confusion matrix:")
-    print(metrics.confusion_matrix(y_test, pred))
+    #print("classification report:")
+    #print(metrics.classification_report(y_test, pred, target_names=target_names))
+    #print("confusion matrix:")
+    #print(metrics.confusion_matrix(y_test, pred))
 
-    print()
+    #print()
     clf_descr = str(clf).split('(')[0]
-    return clf_descr, score, train_time, test_time
+    #return clf_descr, score, train_time, test_time
+    t0 = time()
+    y_pred = cross_val_predict(clf, X, dataset.target, cv=5)
+    train_time = time() - t0
+    
+    print("classification report:")
+    print(metrics.classification_report(dataset.target, y_pred, target_names=target_names))
+
+    print("confusion matrix:")
+    print(metrics.confusion_matrix(dataset.target, y_pred))
+
+    score = metrics.accuracy_score(dataset.target, y_pred)
+    print("accuracy:   %0.3f" % score)
+    return clf_descr, score, train_time
 
 results = []
+#for clf, name in (
+#        (RidgeClassifier(tol=1e-2, solver="lsqr"), "Ridge Classifier"),
+#        (Perceptron(n_iter=50), "Perceptron"),
+#        (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive"),
+#        (KNeighborsClassifier(n_neighbors=10), "kNN"),
+#        (RandomForestClassifier(n_estimators=100), "Random forest")):
+#    print('=' * 80)
+#    print(name)
+#    results.append(benchmark(clf))
 for clf, name in (
         (RidgeClassifier(tol=1e-2, solver="lsqr"), "Ridge Classifier"),
         (Perceptron(n_iter=50), "Perceptron"),
-        (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive"),
         (KNeighborsClassifier(n_neighbors=10), "kNN"),
         (RandomForestClassifier(n_estimators=100), "Random forest")):
     print('=' * 80)
@@ -159,28 +160,28 @@ for penalty in ["l2", "l1"]:
                                        tol=1e-3)))
 
     # Train SGD model
-    results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
-                                           penalty=penalty)))
+    #results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
+    #                                       penalty=penalty)))
 
 # Train SGD with Elastic Net penalty
-print('=' * 80)
-print("Elastic-Net penalty")
-results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
-                                       penalty="elasticnet")))
+#print('=' * 80)
+#print("Elastic-Net penalty")
+#results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
+#                                       penalty="elasticnet")))
 
 # Train NearestCentroid without threshold
-print('=' * 80)
-print("NearestCentroid (aka Rocchio classifier)")
-results.append(benchmark(NearestCentroid()))
+#print('=' * 80)
+#print("NearestCentroid (aka Rocchio classifier)")
+#results.append(benchmark(NearestCentroid()))
 
 # Train sparse Naive Bayes classifiers
 print('=' * 80)
 print("Naive Bayes")
 results.append(benchmark(MultinomialNB(alpha=.01)))
-results.append(benchmark(BernoulliNB(alpha=.01)))
+#results.append(benchmark(BernoulliNB(alpha=.01)))
 
-print('=' * 80)
-print("LinearSVC with L1-based feature selection")
+#print('=' * 80)
+#print("LinearSVC with L1-based feature selection")
 # The smaller C, the stronger the regularization.
 # The more regularization, the more sparsity.
 results.append(benchmark(Pipeline([
@@ -192,18 +193,23 @@ results.append(benchmark(Pipeline([
 
 indices = np.arange(len(results))
 
-results = [[x[i] for x in results] for i in range(4)]
+results = [[x[i] for x in results] for i in range(3)]
 
-clf_names, score, training_time, test_time = results
+#clf_names, score, training_time, test_time = results
+#training_time = np.array(training_time) / np.max(training_time)
+#test_time = np.array(test_time) / np.max(test_time)
+
+clf_names, score, training_time = results
 training_time = np.array(training_time) / np.max(training_time)
-test_time = np.array(test_time) / np.max(test_time)
 
 plt.figure(figsize=(12, 8))
 plt.title("Score")
+#plt.barh(indices, score, .2, label="score", color='navy')
+#plt.barh(indices + .3, training_time, .2, label="training time",
+#         color='c')
+#plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
 plt.barh(indices, score, .2, label="score", color='navy')
-plt.barh(indices + .3, training_time, .2, label="training time",
-         color='c')
-plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
+plt.barh(indices + .3, training_time, .2, label="training time", color='darkorange')
 plt.yticks(())
 plt.legend(loc='best')
 plt.subplots_adjust(left=.25)
